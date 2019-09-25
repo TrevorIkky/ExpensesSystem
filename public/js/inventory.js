40ed615b3a7ef513dab01a4adf1a3ed1f7f987de
JoelOdhiambo <wesleyjoel4@gmail.com>
2019-09-25 09:34:21 +0300

    Inventory Update

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
-    $("#inventory-search").on("keyup",function(){
-    var value =$(this).val().toLowerCase();
-    $("#inventory-table tr").filter(function(){
-    $(this).toggle($(this).text().toLowerCase().indexOf(value)>-1)
-    });
-    });
-    function view(){
+function search() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("inventory-search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("inventory-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+function view(){
         var x=document.getElementById("InventoryTable");
         if(x.style.display=="none"){
             x.style.display="block";
@@ -13,23 +25,4 @@ $(document).ready(function(){
             x.style.display="none";
         }
     }
-    function search() {
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("inventory-search");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("Inventory-Table");
-        tr = table.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-          td = tr[i].getElementsByTagName("td")[0];
-          if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              tr[i].style.display = "";
-            } else {
-              tr[i].style.display = "none";
-            }
-          }       
-        }
-      }
 
-    });
--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -126,39 +126,8 @@
 </table>
 </div>
 <script type="text/javascript" src="{{url ('js/inventory.js') }}" defer></script>
-
 <script type="text/javascript" src = "/js/inventory.js"></script>
-<script>
-  function search() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("inventory-search");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("inventory-table");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
 
-function view(){
-        var x=document.getElementById("InventoryTable");
-        if(x.style.display=="none"){
-            x.style.display="block";
-        }else{
-            x.style.display="none";
-        }
-    }
-
-
-  </script>
 </body>
 
 
